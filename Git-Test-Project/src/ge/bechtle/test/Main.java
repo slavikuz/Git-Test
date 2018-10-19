7485548e8d4596b6db8d403872202b82f7cd8744
yaroslav.shargorodsk <yaroslav.shargorodsk@SHDEBONL232.bechtle.net>
2018-10-19 15:23:57 +0200

    add

--- Git-Test-Project/src/ge/bechtle/test/Main.java
+++ Git-Test-Project/src/ge/bechtle/test/Main.java
@@ -3,7 +3,7 @@ package ge.bechtle.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from  Git Hub!!!");
+		System.out.println("Hello from  Git Hub!!!!");
 
 	}
 
